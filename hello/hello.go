6314f4173f9f313b81898059e81784458ccea0db
chenyangdream <chenyangseamoon@163.com>
2017-12-28 22:38:17 -0500

    打印math.Pi

--- hello/hello.go
+++ hello/hello.go
@@ -1,12 +1,20 @@
 package main
 
-import "fmt"
-import "github.com/chenyangdream/stringutil"
+import (
+	"fmt"
+	"github.com/chenyangdream/stringutil"
+	"math"
+)
 
 var i, j int = 1, 2
+
+func printPi() {
+	fmt.Println(math.Pi)
+}
 
 func main() {
 	var c, python, java = true, false, "no!"
 	fmt.Println(i, j, c, python, java)
 	fmt.Printf(stringutil.Reverse("Hello, world.\n"))
+	printPi()
 }
