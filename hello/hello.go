0a6f39d14d607c8070d192a12012d0b4f858abf0
root <root@host.localdomain>
2017-12-28 22:19:15 -0500

    添加变量并答应

--- hello/hello.go
+++ hello/hello.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 import "github.com/chenyangdream/stringutil"
 
+var i, j int = 1, 2
+
 func main() {
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
 	fmt.Printf(stringutil.Reverse("Hello, world.\n"))
 }
